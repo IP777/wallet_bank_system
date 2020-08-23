c56c3330314f096da4580bfc82c9230abe8af39e
IP777 <ivanov_piter@ukr.net>
2020-08-23 13:20:37 +0300

    edit-3

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -3,12 +3,12 @@ import style from "./HomePage.module.css";
 import Header from "../../components/header/Header";
 
 const HomePage = () => {
-	return (
-		<>
-			<Header />
-			<div className={style.text}>Home Page!!!</div>
-		</>
-	);
+    return (
+        <>
+            <Header />
+            <div className={style.text}>Home Page!!!</div>
+        </>
+    );
 };
 
 export default HomePage;
