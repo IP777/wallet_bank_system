e2cdf673046a6d5db745cdf32357e2e7b4fef3fa
IP777 <ivanov_piter@ukr.net>
2020-08-25 11:09:46 +0300

    master-netlify

---
+++ src/components/tmp/LoginComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./LoginComponent.module.css";
+
+const LoginPage = () => {
+	return <div className={style.wrapper}>Login Component</div>;
+};
+
+export default LoginPage;
---
+++ src/components/tmp/RegisterComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./RegisterComponent.module.css";
+
+const RegisterComponent = () => {
+	return <div className={style.wrapper}>Register Component</div>;
+};
+
+export default RegisterComponent;
