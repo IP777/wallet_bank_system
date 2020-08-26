584633101501c829896ac335aa2a6e1a4631db1b
anatolii.parubets <anatolii.parubets@betlab.com>
2020-08-26 21:41:17 +0300

    fix: resolve formating of code by prettier

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import store from "./redux/store.js";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './redux/store.js';
 
 ReactDOM.render(
-
-	<Provider store={store}>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</Provider>,
-	document.getElementById("root")
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 );
