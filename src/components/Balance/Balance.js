6952a4c84d45c58a9e2bbd40aeed0aa7ed883dbd
Illya Kobyliatskiy <flazzygardner1234@gmail.com>
2020-08-28 00:13:57 +0300

    Update Balance.js

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -3,11 +3,9 @@ import style from './Balance.module.css';
 
 export const Balance = () => {
   return (
-    <>
       <div className={style.balance}>
         <p>Баланс</p>
         <span>24 000.00 грн</span>
       </div>
-    </>
   );
 };
