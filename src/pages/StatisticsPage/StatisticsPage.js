b4839f13f87a8cacaff66a20ecb2235d78446f06
IP777 <ivanov_piter@ukr.net>
2020-08-28 11:48:14 +0300

    to uppercase Edit

---
+++ src/pages/StatisticsPage/StatisticsPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./StatisticsPage.module.css";
+
+const StatisticsPage = () => {
+	return <div className={style.text}>Statistics Page!!!</div>;
+};
+
+export default StatisticsPage;
