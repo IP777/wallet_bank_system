3a64bab2f618d156aead8d25a1f7a5ab8ec73cde
IP777 <ivanov_piter@ukr.net>
2020-08-28 13:23:48 +0300

    add component

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+//import style from "./HomePage.module.css";
+import Header from "../../components/Header/Header.Container";
+import TableTransactions from "../../components/TableTransactions/TableTransactions";
+
+const HomePage = () => {
+	return (
+		<>
+			<Header />
+			<TableTransactions />
+		</>
+	);
+};
+
+export default HomePage;
