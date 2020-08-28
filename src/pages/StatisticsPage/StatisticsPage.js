88df3117f9b2687870274a62e4c6dfee8868af79
IP777 <ivanov_piter@ukr.net>
2020-08-28 16:33:19 +0300

    changes

--- src/pages/StatisticsPage/StatisticsPage.js
+++ src/pages/StatisticsPage/StatisticsPage.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import style from './StatisticsPage.module.css';
+//import style from './StatisticsPage.module.css';
+import Statistics from '../../components/Statistics/Statistics';
+import Header from '../../components/Header/Header';
+import Navigation from '../../components/Navigation/Navigation';
 
 const StatisticsPage = () => {
-  return <div className={style.text}>Statistics Page!!!</div>;
+  return (
+    <>
+      <Header />
+      {/* Компонент-заглушка удалить после создания нормального */}
+      <Navigation />
+      {/* ------------------------ */}
+      <Statistics />
+    </>
+  );
 };
 
 export default StatisticsPage;
