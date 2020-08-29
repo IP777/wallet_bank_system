45909df0039b9301236ee4a5488e9e1d3e12fd2d
IP777 <ivanov_piter@ukr.net>
2020-08-29 14:11:03 +0300

    redux edit

--- src/containers/Loginization.js
+++ src/containers/Loginization.js
@@ -1,14 +1,14 @@
 import { connect } from 'react-redux';
-import * as sessionOperations from '../redux/operations/app/sessionOperation';
-import * as sessionSelectors from '../redux/selectors/app/sessionSelectors';
+import { login } from '../redux/operations/app/sessionOperation';
+import { getIsAuthenticated } from '../redux/selectors/app/sessionSelectors';
 import Loginization from '../components/Loginization/Loginization';
 
 const mapStateToProps = (state) => ({
-  authenticated: sessionSelectors.getIsAuthenticated(state),
+  authenticated: getIsAuthenticated(state),
 });
 
 const mapDispatchToProps = {
-  onLogin: sessionOperations.login,
+  onLogin: login,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Loginization);
