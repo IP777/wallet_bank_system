6d9efb7bfdd5accff6842f0cc4224e0383536b88
Valentyn <valentyn28101993@ukr.net>
2020-08-29 12:26:25 +0300

    pie-chart & pie-chart-table

--- src/pages/statisticsPage/StatisticsPage.js
+++ src/pages/statisticsPage/StatisticsPage.js
@@ -4,26 +4,16 @@ import PieChartComponent from "../../components/PieChart/PieChart3";
 import PieChartTable from "../../components/PieChartTable/PieChartTable";
 
 const StatisticsPage = () => {
-  // const chartData = [
-  //   ["Категории", "Сумма"],
-  //   ["a", 12],
-  //   ["b", 5.5],
-  //   ["c", 10],
-  //   ["d", 10],
-  //   ["e", 10],
-  //   ["f", 10],
-  // ];
-
   const chartData = [
-    { title: "One", value: 10, color: "#E38627" },
-    { title: "Two", value: 15, color: "#C13C37" },
-    { title: "Three", value: 10, color: "#6A2135" },
-    { title: "Four", value: 12, color: "#E38627" },
-    { title: "five", value: 5, color: "#D213F2" },
-    { title: "six", value: 5, color: "#C13C37" },
-    { title: "seven", value: 5, color: "#D213F2" },
-    { title: "eight", value: 8, color: "#C13C37" },
-    { title: "eight", value: 8, color: "#D213F2" },
+    { title: "One", value: 10, color: "#E38127" },
+    { title: "Two", value: 15, color: "#C12C37" },
+    { title: "Three", value: 10, color: "#6A2335" },
+    { title: "Four", value: 12, color: "#E38647" },
+    { title: "five", value: 5, color: "#D215F2" },
+    { title: "six", value: 5, color: "#C63C37" },
+    { title: "seven", value: 5, color: "#D273F2" },
+    { title: "eight", value: 8, color: "#C83C37" },
+    { title: "eight", value: 8, color: "#D293F2" },
     { title: "eight", value: 8, color: "#6A2135" },
     { title: "eight", value: 8, color: "#C13C37" },
   ];
